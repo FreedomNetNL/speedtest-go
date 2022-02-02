2f992380a22bf878ac3307c00749425377161875
Lucas van Beek <info@lucasvanbeek.nl>
2022-02-02 17:43:28 +0100

    Fix default settings

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+adduser --quiet --system --home /var/lib/librespeed-go --no-create-home \
+            --group --gecos "LibreSpeed-Go" librespeed
+
 systemctl daemon-reload
 systemctl enable librespeed-go
 systemctl start librespeed-go
